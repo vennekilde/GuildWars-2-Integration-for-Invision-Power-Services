8946ca5def75f1b2c2857c36406cbdce61ffe154
venne <venne@DESKTOP-AOBI5AO>
2016-04-04 12:57:22 +0200

    switched to some more natural keys

--- admin/applications_addon/other/gw2ci/setup/versions/install/sql/shoutbox_mysql_tables.php
+++ admin/applications_addon/other/gw2ci/setup/versions/install/sql/shoutbox_mysql_tables.php
@@ -9,18 +9,18 @@
 
 // Tables
 $TABLE[] = "CREATE TABLE gw2_api_keys (
-	u_id int(11) NOT NULL,
-    u_api_key CHAR(36) NOT NULL,
+	u_id int(11) NOT NULL auto_increment,
+    u_api_key CHAR(36) NOT NULL UNIQUE,
     u_api_key_name varchar(255) NOT NULL,
     u_api_key_permissions varchar(1024) NOT NULL,
-    u_last_success TIMESTAMP NOT NULL,
+    u_last_success TIMESTAMP NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (u_id)
 )";
 
 $TABLE[] = "CREATE TABLE gw2_account (
 	u_id int(11) NOT NULL,
-    a_uuid CHAR(64) NOT NULL,
-    a_username varchar(255) NOT NULL,
+    a_uuid CHAR(64) NOT NULL UNIQUE,
+    a_username varchar(255) NOT NULL UNIQUE,
     a_world varchar(1024) NOT NULL,
     a_created TIMESTAMP NOT NULL,
     a_access TINYINT NOT NULL,
@@ -34,23 +34,22 @@ $TABLE[] = "CREATE TABLE gw2_account (
 
 $TABLE[] = "CREATE TABLE gw2_guild(
     g_uuid CHAR(64) NOT NULL,
-    g_name CHAR(64) NOT NULL,
+    g_name CHAR(64) NOT NULL UNIQUE,
     g_tag varchar(4) NOT NULL,
 	PRIMARY KEY (g_uuid)
 )";
 
 $TABLE[] = "CREATE TABLE gw2_guild_membership(
 	u_id int(11) NOT NULL,
-    g_uuid CHAR(64) NOT NULL,
+    g_uuid CHAR(64) NOT NULL UNIQUE,
 	PRIMARY KEY (u_id, g_uuid),
     FOREIGN KEY (u_id) REFERENCES gw2_api_keys(u_id),
     FOREIGN KEY (g_uuid) REFERENCES gw2_guild(g_uuid)
 )";
 
 $TABLE[] = "CREATE TABLE gw2_characters(
-	c_id int(11) NOT NULL auto_increment,
-    u_id int(11) NOT NULL,
     c_name varchar(128) NOT NULL,
+    u_id int(11) NOT NULL,
     c_race TINYINT NOT NULL,
     c_gender TINYINT(1) NOT NULL COMMENT '0 = Male, 1 = Female',
     c_profession TINYINT NOT NULL,
@@ -59,21 +58,19 @@ $TABLE[] = "CREATE TABLE gw2_characters(
     c_age int(11) NOT NULL,
     c_created TIMESTAMP NOT NULL,
     c_deaths TIMESTAMP NOT NULL,
-	PRIMARY KEY (c_id),
+	PRIMARY KEY (c_name),
     KEY (u_id),
     FOREIGN KEY (u_id) REFERENCES gw2_api_keys(u_id),
     FOREIGN KEY (g_uuid) REFERENCES gw2_guild(g_uuid)
 )";
 
 $TABLE[] = "CREATE TABLE gw2_character_crafting(
-	cr_id int(11) NOT NULL auto_increment,
-    c_id int(11) NOT NULL,
+    c_name varchar(128) NOT NULL,
     cr_dicipline TINYINT NOT NULL,
     cr_rating varchar(128) NOT NULL,
     cr_active TINYINT(1) NOT NULL,
-	PRIMARY KEY (cr_id),
-    KEY (c_id),
-    FOREIGN KEY (c_id) REFERENCES gw2_characters(c_id)
+	PRIMARY KEY (c_name),
+    FOREIGN KEY (c_name) REFERENCES gw2_characters(c_name)
 )"; 
 
 $TABLE[] = "CREATE TABLE gw2_pvp_stats(
@@ -88,11 +85,29 @@ $TABLE[] = "CREATE TABLE gw2_pvp_stats(
     ps_byes int(11) NOT NULL,
     ps_forfeits int(11) NOT NULL,
     
+    ps_ladder_none_wins int(11) NOT NULL,
+    ps_ladder_none_losses int(11) NOT NULL,
+    ps_ladder_none_desertions int(11) NOT NULL,
+    ps_ladder_none_byes int(11) NOT NULL,
+    ps_ladder_none_forfeits int(11) NOT NULL,
+	
     ps_ladder_ranked_wins int(11) NOT NULL,
     ps_ladder_ranked_losses int(11) NOT NULL,
     ps_ladder_ranked_desertions int(11) NOT NULL,
     ps_ladder_ranked_byes int(11) NOT NULL,
     ps_ladder_ranked_forfeits int(11) NOT NULL,
+	
+    ps_ladder_soloarenarated_wins int(11) NOT NULL,
+    ps_ladder_soloarenarated_losses int(11) NOT NULL,
+    ps_ladder_soloarenarated_desertions int(11) NOT NULL,
+    ps_ladder_soloarenarated_byes int(11) NOT NULL,
+    ps_ladder_soloarenarated_forfeits int(11) NOT NULL,
+    
+    ps_ladder_teamarenarated_wins int(11) NOT NULL,
+    ps_ladder_teamarenarated_losses int(11) NOT NULL,
+    ps_ladder_teamarenarated_desertions int(11) NOT NULL,
+    ps_ladder_teamarenarated_byes int(11) NOT NULL,
+    ps_ladder_teamarenarated_forfeits int(11) NOT NULL,
     
     ps_ladder_unranked_wins int(11) NOT NULL,
     ps_ladder_unranked_losses int(11) NOT NULL,
@@ -116,9 +131,8 @@ $TABLE[] = "CREATE TABLE gw2_pvp_profession_stats(
     FOREIGN KEY (u_id) REFERENCES gw2_api_keys(u_id)
 )";
 $TABLE[] = "CREATE TABLE gw2_pvp_games(
-	game_id int(11) NOT NULL auto_increment,
-    u_id int(11) NOT NULL,
 	game_uuid CHAR(64) NOT NULL,
+    u_id int(11) NOT NULL,
 	game_map_id int(11) NOT NULL,
 	game_started TIMESTAMP NOT NULL,
 	game_ended TIMESTAMP NOT NULL,
@@ -127,7 +141,7 @@ $TABLE[] = "CREATE TABLE gw2_pvp_games(
     game_profession TINYINT NOT NULL,
     game_score_red SMALLINT NOT NULL,
     game_score_blue SMALLINT NOT NULL,
-	PRIMARY KEY (game_id),
+	PRIMARY KEY (game_uuid),
     KEY (u_id),
     FOREIGN KEY (u_id) REFERENCES gw2_api_keys(u_id)
 )";
