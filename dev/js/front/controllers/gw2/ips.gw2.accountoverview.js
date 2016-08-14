f48366485c5b6528bd0c13fc7e78d30ebeedba6a
venne <venne@DESKTOP-AOBI5AO>
2016-08-14 02:45:38 +0200

    Now hides altermative acc name and/or guild if shown by gw2integration

---
+++ dev/js/front/controllers/gw2/ips.gw2.accountoverview.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+;( function($, _, undefined){
+	"use strict";
+	
+    ips.controller.register('gw2integration.front.gw2.accountoverview', {
+        initialize: function () {
+            if($(this.scope[0]).attr("data-hide-alt-acc-name") == "1"){
+                $(this.scope[0]).closest(".cAuthorPane").find("li:contains('GW2 Account')").hide();
+            }
+            if($(this.scope[0]).attr("data-hide-alt-guild") == "1"){
+                $(this.scope[0]).closest(".cAuthorPane").find("li:contains('GW2 Account')").hide();
+            }
+		}
+    });
+
+}(jQuery, _));
